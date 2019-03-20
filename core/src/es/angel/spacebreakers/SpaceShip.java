6b5f7cc503b18b40b8636c16bb1b87fec4b0b63a
Angel Mateu Paris <44613791+xSoraSS@users.noreply.github.com>
2019-03-20 19:56:22 +0100

    Update SpaceShip.java

--- core/src/es/angel/spacebreakers/SpaceShip.java
+++ core/src/es/angel/spacebreakers/SpaceShip.java
@@ -1,7 +1,8 @@
 package es.angel.spacebreakers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -9,12 +10,17 @@ public class SpaceShip {
     SpriteBatch batch;
     Texture ship;
     Sprite myShip;
+    int shipX = 270, shipY = 10;
+//    boolean izquierda = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+//    boolean derecha = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+//    boolean mover = (izquierda != derecha);
+
 
     public void create() {
         batch = new SpriteBatch();
         ship = new Texture("ship.png");
         myShip = new Sprite(ship);
-        myShip.setPosition(270, 10);
+        myShip.setPosition(shipX, shipY);
         myShip.setSize(100, 100);
     }
 
@@ -23,6 +29,17 @@ public class SpaceShip {
         myShip.draw(batch);
         batch.end();
     }
+
+    void moveNave(){
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            create();
+                shipX--;
+        }else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            create();
+                shipX++;
+            }
+        }
 
     void dispose() {
         batch.dispose();
